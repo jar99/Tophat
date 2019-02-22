921dd371c9e432dcd056a38c2c70e68d3c6ddf00
Megalovania <coryciz@gmail.com>
2019-02-22 18:48:12 -0500

    Built The JavaFX Railway Template
    If testing, be sure to start from Main.c

---
+++ Tophat-Railway/src/application/CTC/CTCSingleton.java
@@ -0,0 +1,52 @@
+package application.CTC;
+
+import application.MBO.MBOSingleton;
+import application.TrackController.TrackControllerSingleton;
+import application.TrackModel.TrackModelSingleton;
+import application.TrainController.TrainControllerSingleton;
+import application.TrainModel.TrainModelSingleton;
+
+public class CTCSingleton {
+
+	// Singleton Functions (NO TOUCHY!!)
+	private static CTCSingleton instance = null;
+
+	private CTCSingleton() {
+	}
+
+	public static CTCSingleton getInstance() {
+		if (instance == null) {
+			instance = new CTCSingleton();
+		}
+
+		return instance;
+	}
+
+	// =====================================
+
+	// NOTE: Put your data objects here
+	private int count = 0;
+
+	// NOTE: Put some functions here
+	public void increment() {
+		count++;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	// NOTE: Singleton Connections (Put changes reads, gets, sets that you want to
+	// occur here)
+	// WARNING: This Only changes the singleton, not your UI. UI updates occur in
+	// your UI controller
+	public void update() {
+		// Example: get the count from a singleton and replace yours with the largest
+		MBOSingleton mboModSin = MBOSingleton.getInstance();
+		int t_count = mboModSin.getCount();
+		if (count < t_count)
+			count = t_count;
+
+	}
+
+}
--- Tophat-Railway/src/application/Main.java
+++ Tophat-Railway/src/application/Main.java
@@ -1,26 +1,60 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import application.MBO.MBOSingleton;
+import application.TrackController.TrackControllerSingleton;
+import application.TrackModel.TrackModelSingleton;
+import application.TrainController.TrainControllerSingleton;
+import application.TrainModel.TrainModelSingleton;
+import application.CTC.CTCSingleton;
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+// WARNING: You MUST run this program from Main.java, 
+// 		otherwise the Singleton's won't talk to each other
+public class Main {
+
 	public static void main(String[] args) {
-		launch(args);
+		// Singleton References
+		CTCSingleton 				ctcSin = 		CTCSingleton.getInstance();
+		TrackControllerSingleton 	tckCtrlSin = 	TrackControllerSingleton.getInstance();
+		TrackModelSingleton 		tckModSin = 	TrackModelSingleton.getInstance();
+		TrainModelSingleton 		trnModSin = 	TrainModelSingleton.getInstance();
+		TrainControllerSingleton 	trnCtrlSin = 	TrainControllerSingleton.getInstance();
+		MBOSingleton 				mboSin = 		MBOSingleton.getInstance();
+
+		// Calling Thread which starts the FX UI
+		new Thread() {
+			@Override
+			public void run() {
+				javafx.application.Application.launch(UIApp.class);
+			}
+		}.start();
+		UIApp.waitForUITest();
+
+		// Scheduler which tells the singleton's to update
+		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				update(ctcSin, tckCtrlSin, tckModSin, trnModSin, trnCtrlSin, mboSin);
+			}
+		}, 0, 1, TimeUnit.SECONDS); // Determines update frequency (1 sec atm)
+
+	}
+
+	// calls each singleton's update() method
+	private static void update(CTCSingleton ctcSin, TrackControllerSingleton tckCtrlSin, TrackModelSingleton tckModSin,
+			TrainModelSingleton trnModSin, TrainControllerSingleton trnCtrlSin, MBOSingleton mboSin) {
+		// call singleton update methods
+		ctcSin.update();
+		tckCtrlSin.update();
+		tckModSin.update();
+		trnModSin.update();
+		trnCtrlSin.update();
+		mboSin.update();
+
 	}
 }
