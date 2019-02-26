4b9d6d0040d529080d7250866292bca253ddcfd9
zhangjy97@sjtu.edu.cn <zhangjy97@sjtu.edu.cn>
2019-02-26 08:33:58 +0800

    CTC1.0

--- Tophat-Railway/src/application/CTC/CTCCtrl.java
+++ Tophat-Railway/src/application/CTC/CTCCtrl.java
@@ -2,12 +2,15 @@ package application.CTC;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.collections.*;
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-
+import javafx.scene.text.Text;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
 public class CTCCtrl implements Initializable {
 
 	// Links to your Singleton (NO TOUCHY!!)
@@ -20,20 +23,61 @@ public class CTCCtrl implements Initializable {
 	// WARNING: Your fx:id and variable name Must Match!
 	// Links to FXML elements
 	@FXML
-	private Label counter;
-
+	private TextField TrainIDTextField;
+	@FXML
+	private TextField SpeedTextField;
+	@FXML
+    private TextField DepartureTimeText;
+	@FXML
+	private ChoiceBox<String> DepartureStationChoiceBox;
+	@FXML
+	private ChoiceBox<String> LineChoiceBox;
+	@FXML
+	private ChoiceBox<String> DestinationChoiceBox;
+    @FXML
+	private ListView<String> ScheduleListView;
+	@FXML
+	private ListView<String> ManagementListView;
+	@FXML
+    private TextField IDModify;
+    @FXML
+    private TextField AuthorityModify;
+    @FXML
+    private TextField SpeedModify;
 	// NOTE: This is where you build UI functionality
 	// functions can be linked through FX Builder or manually
 	// Control Functions
-	public void buttonClicked() {
-		// System.out.print("Click");
-		mySin.increment();
-	}
+	public void ButtonCreateTrainClicked() {
+		if (mySin.addTrain(TrainIDTextField.getCharacters().toString(),SpeedTextField.getCharacters().toString())){
+			System.out.print("Successful");//TODO Change this into UI
+		}
+		else return;
+		String departure=DepartureStationChoiceBox.getSelectionModel().getSelectedItem();
+		String line=LineChoiceBox.getSelectionModel().getSelectedItem();
+		String destination=DestinationChoiceBox.getSelectionModel().getSelectedItem();
+		String departuretime=DepartureTimeText.getCharacters().toString();
+		String[] routine={"STATIONA","STATIONB"};
+		mySin.addSchedule(Integer.parseInt(TrainIDTextField.getCharacters().toString()),line,routine);
+		ObservableList<String> ScheduleString = FXCollections.observableArrayList(mySin.tolist());
+		ScheduleListView.setItems(ScheduleString);
+		ObservableList<String> TrainString = FXCollections.observableArrayList(mySin.tolistTrains());
+		ManagementListView.setItems(TrainString);
 
+	}
+	public void ModifyClicked() {
+		String ID=IDModify.getCharacters().toString();
+		mySin.ModifyTrain(Integer.valueOf(ID),Integer.parseInt(AuthorityModify.getCharacters().toString()),Integer.parseInt(SpeedModify.getCharacters().toString()));
+		ObservableList<String> TrainString = FXCollections.observableArrayList(mySin.tolistTrains());
+		ManagementListView.setItems(TrainString);
+    }
 	// Starts the automatic update (NO TOUCHY!!)
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		DepartureStationChoiceBox.setItems(FXCollections.observableArrayList("STATIONA", "STATIONB"));
+		//TODO load info from trackmodel
+		LineChoiceBox.setItems(FXCollections.observableArrayList("Green", "Red"));
+		DestinationChoiceBox.setItems(FXCollections.observableArrayList("STATIONC", "STATIOND"));
+		//TODO load info from trackmodel
 		updateAnimation = new AnimationTimer() {
 
 			@Override
@@ -49,8 +93,10 @@ public class CTCCtrl implements Initializable {
 	// You can read/change fx elements linked above
 	// WARNING: This assumes your singleton is updating its information
 	private void update() {
-		int count = mySin.getCount();
-		counter.setText(Integer.toString(count));
+		//DepartureStationChoiceBox.setItems(FXCollections.observableArrayList("STATIONA", "STATIONB"));
+		ObservableList<String> TrainString = FXCollections.observableArrayList(mySin.tolistTrains());
+		ManagementListView.setItems(TrainString);
+
 
 	}
 }
--- Tophat-Railway/src/application/CTC/CTCSingleton.java
+++ Tophat-Railway/src/application/CTC/CTCSingleton.java
@@ -1,5 +1,8 @@
 package application.CTC;
-
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Map.Entry;
 import application.MBO.MBOSingleton;
 import application.TrackController.TrackControllerSingleton;
 import application.TrackModel.TrackModelSingleton;
@@ -25,28 +28,147 @@ public class CTCSingleton {
 	// =====================================
 
 	// NOTE: Put your data objects here
-	private int count = 0;
-
+	private Map<Integer,Train> trains =new HashMap<Integer,Train>();
+	private ArrayList<Schedule> myschedule=new ArrayList<Schedule>();
 	// NOTE: Put some functions here
-	public void increment() {
-		count++;
+	public boolean addTrain(String ID,String Speed){
+		Pattern pattern = Pattern.compile("[0-9]*");
+		Matcher isNum1 = pattern.matcher(ID);
+		Matcher isNum2 = pattern.matcher(Speed);
+		if((!isNum1.matches())||(!isNum2.matches())){
+			System.out.println("you should input an integer for ID and Speed!!");//TODO change this into UI
+			return false;
+		}
+		if (trains.containsKey(Integer.valueOf(ID))){
+			System.out.println("Duplicated train ID!!");//TODO change this into UI
+			return false;
+		}
+		trains.put(Integer.valueOf(ID), new Train(Integer.parseInt(ID), Integer.parseInt(Speed)));
+		return true;
+		
 	}
-
-	public int getCount() {
-		return count;
+	public boolean addSchedule(int ID, String myLine, String[] myStation){
+		Schedule tmp=new Schedule(ID, myLine, myStation);
+		myschedule.add(tmp);
+		return true;
 	}
-
+	public ArrayList<String> tolist(){
+		ArrayList<String> tmp=new ArrayList<String>();
+		for (Schedule s:myschedule){
+			tmp.addAll(s.printschedule());
+		}
+		return tmp;
+	}
+	public ArrayList<String> tolistTrains(){
+		ArrayList<String> tmp=new ArrayList<String>();
+		Iterator<Entry<Integer, Train>> iterator = trains.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Entry<Integer, Train> entry = iterator.next();
+			Train value = entry.getValue();
+			tmp.addAll(value.printTrain());
+		}
+		return tmp;
+	}
+	public void ModifyTrain(Integer ID, int Authority, int Speed){
+		Train tmp=trains.get(ID);
+		tmp.set2(Speed, Authority);
+	}
+	public Map<Integer,Train> viewtrains(){
+		return trains;
+	}
+	public ArrayList<Schedule> viewSchedule(){
+		return myschedule;
+	}
 	// NOTE: Singleton Connections (Put changes reads, gets, sets that you want to
 	// occur here)
 	// WARNING: This Only changes the singleton, not your UI. UI updates occur in
 	// your UI controller
 	public void update() {
-		// Example: get the count from a singleton and replace yours with the largest
-		MBOSingleton mboModSin = MBOSingleton.getInstance();
-		int t_count = mboModSin.getCount();
-		if (count < t_count)
-			count = t_count;
+		/*
+		MBOSingleton mboModSin=MBOSingleton.getInstance();
+		TrackModelSingleton tkmModSin=TrackModelSingleton.getInstance();
+		TrackControllerSingletom TkCtrlSin=TrackControllerSingletom.getInstance();
+		*/
+
 
 	}
 
+}
+class Train{
+	private int ID;
+	private int SuggestedSpeed;
+	private int authority=0;
+	private double CurrentSpeed=0;
+	private double CurrentPosition=0;
+	Train(int anID,int aSpeed){
+		ID=anID;
+		SuggestedSpeed=aSpeed;
+	}
+	public void set(double aSpeed, double aPosition){
+		CurrentSpeed=aSpeed;
+		CurrentPosition=aPosition;
+	}
+	public void set2(int speed, int anauthority){
+		SuggestedSpeed=speed;
+		authority=anauthority;
+	}
+	public int getID(){
+		return ID;
+	}
+	public int getSuggestedSpeed(){
+		return SuggestedSpeed;
+	}
+	public double getCurrentSpeed(){
+		return CurrentSpeed;
+	}
+	public double getCurrentPosition(){
+		return CurrentPosition;
+	}
+	public int getAuthority(){
+		return authority;
+	}
+	public ArrayList<String> printTrain(){
+		ArrayList<String> tmp=new ArrayList<String>();
+		tmp.add(String.valueOf(ID));
+		tmp.add("	Current Speed:"+CurrentSpeed);
+		tmp.add("	Current Position:"+ CurrentPosition);
+		tmp.add("	Authority:"+authority);
+		return tmp;
+	}
+}
+class Schedule{
+	private int ID;
+	private String Line;
+	private String[] Station;
+	private int[] ArrivalTime;
+	private int[] LeaveTime;
+	Schedule(int TrainID, String myLine, String[] myStation){
+		ID=TrainID;
+		Line=myLine;
+		Station=myStation;
+	}
+	public ArrayList<String> printschedule(){
+		ArrayList<String> tmp=new ArrayList<String>();
+		tmp.add(String.valueOf(ID));
+		tmp.add("	"+Line);
+		for (String s:Station){
+			tmp.add("	"+s+" "+"time1 "+"time2");//TODO 
+		}
+		return tmp;
+	}
+	public int getID(){
+		return ID;
+	}
+	public String getLine(){
+		return Line;
+	}
+	public String[] getStation(){
+		return Station;
+	}
+	public int[] getArrivalTime(){
+		return ArrivalTime;
+	}
+	public int[] getLeaveTime(){
+		return LeaveTime;
+	}
 }
