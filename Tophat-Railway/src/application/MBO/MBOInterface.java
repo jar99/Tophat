2e7ac4a4c6896af2383b0298b3e16ee708072f46
MattSachenik <sachenikmatt@gmail.com>
2019-04-01 16:52:50 -0400

    whoops

--- Tophat-Railway/src/application/MBO/MBOInterface.java
+++ Tophat-Railway/src/application/MBO/MBOInterface.java
@@ -1,13 +1,5 @@
 package application.MBO;
 
-import application.TrackModel.TrackLine;
-
 public interface MBOInterface {
 
-	/**
-	 * Gives MBO a new Line
-	 * 
-	 * @param trackLine - deep copy of the new line object with all it's data
-	 */
-	public void createTrain(TrackLine trackLine);
 }
