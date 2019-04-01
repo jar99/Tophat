4ce495743b7f84d29a1cab552fc8099b98725a28
MattSachenik <sachenikmatt@gmail.com>
2019-04-01 16:54:48 -0400

    Interface fix

--- Tophat-Railway/src/application/MBO/MBOInterface.java
+++ Tophat-Railway/src/application/MBO/MBOInterface.java
@@ -1,5 +1,13 @@
 package application.MBO;
 
+import application.TrackModel.TrackLine;
+
 public interface MBOInterface {
 
+	/**
+	 * Gives MBO a new Line
+	 * 
+	 * @param trackLine - deep copy of the new line object with all it's data
+	 */
+	public void createTrain(TrackLine trackLine);
 }
