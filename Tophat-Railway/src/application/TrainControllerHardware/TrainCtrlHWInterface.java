27491c24f87e58fd7bc71895b175e2b51033f092
Roman Hamilton <romanhamilton@gmail.com>
2019-04-05 16:41:17 -0400

    Added TC HW interface

---
+++ Tophat-Railway/src/application/TrainControllerHardware/TrainCtrlHWInterface.java
@@ -0,0 +1,17 @@
+package application.TrainControllerHardware;
+
+public interface TrainCtrlHWInterface {
+	public int getTrainID();
+	public double getPower();
+	public double getSpeed();
+	public boolean getLights();
+	public double getTemperature();
+	public boolean getLeftDoor();
+	public boolean getRightDoor();
+	public boolean getDriveMode();
+	public boolean getServiceBrake();
+	public boolean getEmergencyBrake();
+	public boolean getEngineState();
+	public boolean getBrakeState();
+	public boolean getSignalState();
+}
