f032aecedda4a192d9ac09349aa67c6b538a3cc4
Roman Hamilton <romanhamilton@gmail.com>
2019-04-09 15:47:55 -0400

    Added all getters and setters to singleton

--- Tophat-Railway/src/application/TrainControllerHardware/TrainControllerHWSingleton.java
+++ Tophat-Railway/src/application/TrainControllerHardware/TrainControllerHWSingleton.java
@@ -8,6 +8,7 @@ import application.TrainModel.TrainInterface;
 public class TrainControllerHWSingleton{
 
 	private static TrainControllerHWSingleton instance = null;
+	private static TrainModelSingleton trnModelSin = TrainModelSingleton.getInstance();
 	
 	/**
 	 * Private constructor that makes this a singleton class.
@@ -25,12 +26,94 @@ public class TrainControllerHWSingleton{
 	private boolean drivingMode;
 	private double speed, power, ki, kp;
 	private boolean brake, eBrake;
-	private boolean lights, lDoor, rDoor, temp;
+	private boolean lights, leftDoor, rightDoor;
+	private double temp;
+	
+	// values that I'm just displaying
+	private int mboAuthority, trackAuthority;
+	private double mboSpeed, trackSpeed, actualSpeed;
+	private boolean engineState, signalState, brakeState;
+	
+	// getters and setters
+	public int getTrainId() {
+		return trainId;
+	}
+	
+	void setTrainId(int id) {
+		trainId = id;
+	}
+	
+	public boolean getDrivingMode() {
+		return drivingMode;
+	}
+	
+	void setDrivingMode(boolean mode) {
+		drivingMode = mode;
+	}
+	
+	void setKi(double newKi) {
+		ki = newKi;
+	}
+	
+	void setKp(double newKp) {
+		kp = newKp;
+	}
+	
+	void setSpeed(double newSpeed) {
+		speed = newSpeed;
+	}
+	
+	void setTemperature(double newTemp) {
+		temp = newTemp;
+	}
+	
+	void toggleServiceBrake() {
+		brake = !brake;
+	}
+	
+	void triggerEmergencyBrake() {
+		eBrake = true;
+	}
+	
+	void resetEmergencyBrake() {
+		eBrake = false;
+	}
+	
+	void toggleLights() {
+		lights = !lights;
+	}
+	
+	void toggleLeftDoor() {
+		leftDoor = !leftDoor;
+	}
+	
+	void toggleRightDoor() {
+		rightDoor = !rightDoor;
+	}
 	
 	/**
 	 * Controls Singleton connections, doesn't update UI
 	 */
 	public void update(){
+		TrainInterface train = trnModelSin.getTrain(trainId);
+		if(train == null) return;
 		
+		mboAuthority = train.getMBOAuthority();
+		trackAuthority = train.getTrackAuthority();
+		mboSpeed = train.getMBOSpeed();
+		trackSpeed = train.getTrackSpeed();
+		actualSpeed = train.getSpeed();
+		engineState = train.engineState();
+		signalState = train.railSignalState();
+		// brakeState = train.brakeState();
+		
+		train.setPower(power);
+		// train.setTemperature(temp);
+		if(train.getServiceBrake() != brake) train.setServiceBrake();
+		if(!train.getEmergencyBrake() && eBrake) train.triggerEmergencyBrake();
+		if(train.getEmergencyBrake() && !eBrake) train.resetEmergencyBrake();
+		if(train.getLightState() != lights) train.toggleLights();
+		if(train.getLeftDoorState() != leftDoor) train.toggleLeftDoors();
+		if(train.getRightDoorState() != rightDoor) train.toggleRightDoors();
 	}
 }
