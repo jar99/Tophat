875081b45c9e0edd174f8c09eca63e170bf49343
Roman Hamilton <romanhamilton@gmail.com>
2019-04-09 15:48:30 -0400

    Screen works!

--- Tophat-Railway/src/application/TrainControllerHardware/ArduinoController/ArduinoController.ino
+++ Tophat-Railway/src/application/TrainControllerHardware/ArduinoController/ArduinoController.ino
@@ -1,6 +1,20 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+// declare OLED display connected to I2C
+#define SCREEN_RESET 4
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  // clear buffer
+  display.clearDisplay();
 }
 
 void loop() {
