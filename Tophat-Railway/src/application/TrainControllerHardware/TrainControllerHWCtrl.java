94ae61689936d2ca2632513f919a799537bcfe2b
Roman Hamilton <romanhamilton@gmail.com>
2019-04-09 10:07:13 -0400

    Changed my filenames for consistency, added FXML file

--- Tophat-Railway/src/application/TrainControllerHardware/TrainControllerHWCtrl.java
+++ Tophat-Railway/src/application/TrainControllerHardware/TrainControllerHWCtrl.java
@@ -4,7 +4,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-public class TrainControllerHardwareCtrl implements Initializable{
+/**
+ * Controller for train controller hardware UI. UI for this module simply shows data, all data is
+ * modified via the connected Arduino.
+ * @author Roman
+ * @version 1.0.0
+ */
+public class TrainControllerHWCtrl implements Initializable{
 
 	// link to singleton
 	private TrainControllerHardwareSingleton mySin = TrainControllerHardwareSingleton.getInstance();
