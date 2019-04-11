e7f39a5bf553e611d39482542046b682294fd2b3
Roman Hamilton <romanhamilton@gmail.com>
2019-04-10 23:13:46 -0400

    WHy didn't this update

--- Tophat-Railway/src/application/TrainControllerHardware/ArduinoController/ArduinoController.ino
+++ Tophat-Railway/src/application/TrainControllerHardware/ArduinoController/ArduinoController.ino
@@ -15,7 +15,6 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET);
 #define NAV_B 3
 #define NAV_C 4
 #define NAV_D 5
-#define NAV_BUTTON 6
 #define KP_1 7
 #define KP_2 8
 #define KP_3 9
@@ -24,12 +23,24 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET);
 
 // globals (I know they're bad, leave me alone)
 int state = 0; // 0 for picking value to change, 1 for changing value, 2 for confirming
-int current_value = 0; // 0 for speed
+int current_value = 0; // 0 for speed, 1 for ki, 2 for kp, 3 for temperature, 4 for service brake, 5 for lights, 6 for left door, 7 for right door,
+// 8 for emergency brake
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("DONT STOP THIS TRAIN");
+  pinMode(NAV_A, INPUT_PULLUP);
+  //attachPCINT(digitalPinToPCINT(NAV_A), ..., FALLING);
+  pinMode(NAV_B, INPUT_PULLUP);
+  //attachPCINT(digitalPinToPCINT(NAV_B), ..., FALLING);
+  pinMode(NAV_C, INPUT_PULLUP);
+  //attachPCINT(digitalPinToPCINT(NAV_C), ..., FALLING);
+  pinMode(NAV_D, INPUT_PULLUP);
+  //attachPCINT(digitalPinToPCINT(NAV_D), ..., FALLING);
 
+  pinMode(EBRAKE, INPUT_PULLUP);
+  attachPCINT(digitalPinToPCINT(EBRAKE), STOPTRAIN, FALLING);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
   // clear buffer
@@ -48,9 +59,22 @@ void setup() {
   display.display();
 }
 
+
+
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if(state){
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println(F("HOLY SHIT STOP THIS FUCKING TRAIN"));
+    display.display();
+    delay(100);
+  }
+  else{
+    display.clearDisplay();
+    display.display();
+  }
   //  "select property to edit"
   //  scroll through values with nav switch and select one by clicking
   //  depending on value, it will be input in some way
@@ -59,6 +83,12 @@ void loop() {
 }
 
 // ISRs
+void STOPTRAIN(){
+  Serial.print("HOLY SHIT STOP THIS FUCKING TRAIN");
+  Serial.println(state);
+  if(state) state = 0;
+  else state = 1;
+}
 void nav_up(){
   
 }
